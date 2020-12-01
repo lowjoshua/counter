7330b493c2afa5efca598ca4a49aa16478045bae
Joshua Low <Joshua Low>
2020-12-01 13:35:23 +0800

    3 buttons and rules

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  let [count, setCount] = useState(0)
+
+  function Increment() {
+    console.log("You pressed me ouch!");
+    setCount(count + 1);
+  }
+  function Decrement() {
+    if (count > 0) {
+    console.log("You pressed me ouch!");
+    setCount(count -1);
+    }
+  } 
+  function Reset() {
+    console.log("You pressed me ouch!");
+    setCount(count = 0);
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{count}</Text>
+      <Button title='Increment' onPress={Increment}> </Button>
+      <Button title='Decrement' onPress={Decrement}> </Button>
+      <Button title='Reset' onPress={Reset}> </Button>
       <StatusBar style="auto" />
     </View>
   );
